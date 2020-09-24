ad3196fdc30260ecc28d3950c750e95f7c1f64b9
Laura Lujan <lauralujanhdez@gmail.com>
2020-09-23 20:04:20 -0500

    includes play button, movements buttons and scores

--- script.js
+++ script.js
@@ -5,75 +5,126 @@ let win = 0,
   lose = 0,
   tie = 0;
 
-// Create a function for the computer play which gives a random movement
-function computerPlay(arr) {
+
+//This function adds the no display class to elements that we dont want to be shown on the dom
+function addClassName (id, cName){
+  const elem = document.getElementById(id);
+  elem.classList.add(cName)
+  console.log(elem);
+}
+
+//this function removes a class name is used to show elements on the dom
+ function removeClassName(clas){
+   const elem = document.getElementsByClassName(clas);
+   elem[0].classList.remove(clas);
+ }
+
+ //this function is called when the user hits play button and calls the functions for display and hide 
+ function play(){
+   removeClassName('no-display');
+   addClassName('img', 'no-display');
+   addClassName('play-btn', 'no-display')
+   removeClassName('hidden');
+   removeClassName('hidden');
+   removeClassName('hidden');
+ }
+
+//get player movement
+function playerMove(e){
+  const move = e.target.value;
+  console.log(move)
+  return move
+}
+
+//get computer movement
+function computerMove(arr) {
   let computerOption = Math.floor(Math.random() * arr.length);
   return arr[computerOption];
 }
 
-// Create a function to get the user movement
-function playerMovement() {
-  let playerOption = prompt(
-    "Write your movement 'rock', 'paper' or 'scissors'"
-  );
-  playerOption = playerOption.toLocaleLowerCase();
+//Single play round gets player and computer movements and determines who is the round's winer
 
-  // compares if the player option is included on the options
-  if (!options.includes(playerOption)) {
-    throw new Error("Please write a valid movement");
-  }
-  return playerOption;
-}
-
-//Create single round play
 function singleRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     tie += 1;
+    showScore('#ties-score', tie);
+    showScore('#legend', "It's a tie");
     return "It's a tie";
   } else {
     switch (playerSelection) {
       case "rock":
         if (computerSelection === "scissors") {
           win += 1;
+          showScore('#player-counter', win);
+          showScore('#legend', "You win! Rock beats Scissors");
           return "You win! Rock beats Scissors";
         } else {
           lose += 1;
+          showScore('#computer-counter', lose);
+          showScore('#legend', "You lose! Paper beats Rock");
           return "You lose! Paper beats Rock";
         }
         break;
       case "paper":
         if (computerSelection === "rock") {
           win += 1;
+          showScore('#player-counter', win);
+          showScore('#legend', "You win! Paper beats Rock");
           return "You win! Paper beats Rock";
         } else {
           lose += 1;
+          showScore('#computer-counter', lose);
+          showScore('#legend', "You lose! Scissors beats Paper");
           return "You lose! Scissors beats Paper";
         }
         break;
       case "scissors":
         if (computerSelection === "paper") {
           win += 1;
+          showScore('#player-counter', win);
+          showScore('#legend', "You win! Scissors beats Paper");
           return "You win! Scissors beats Paper";
         } else {
           lose += 1;
+          showScore('#computer-counter', lose);
+          showScore('#legend',  "You lose! Rock beats Scissors");
           return "You lose! Rock beats Scissors";
         }
         break;
     }
   }
 }
-//Game, create  a function that plays the single round five times and determines the winer
+
 function game() {
-  for (let rounds = 0; rounds < 5; rounds++) {
-    console.log(singleRound(playerMovement(), computerPlay(options)));
+   singleRound(playerMove(event), computerMove(options));
+   
+   its_over(win, lose);
+
+}
+
+function its_over(player, computer) {
+  if (player === 5) {
+    newDiv("you're the winner!!");
   }
-  if (win > lose && win > tie) {
-    alert("You are the winner");
-  } else if (lose > win && lose > tie) {
-    alert("You lost");
-  } else {
-    alert("It's a tie");
+
+  if (computer === 5) {
+   newDiv("you lose !!");
   }
 }
 
-game();
+
+// this function is used include the scores of the players
+function showScore(id, content){
+  const p = document.querySelector(id);
+  p.textContent = content;
+}
+
+//function to create new div to show the game results
+function newDiv(text){
+  const main = document.querySelector("main");
+  const div = document.createElement('div');
+  div.classList.add('game-results');
+  div.textContent(text);
+
+  main.appendChild(div);
+}
